84b7ff3782cf765f11893dc93ba8daff2ad5a32e
BunchaNumbers <jacobhuangyuning@msn.com>
2015-09-12 19:45:51 -0400

    Search is working :X need to impliment filter

--- TheTDotExchange/app/src/main/java/com/example/android/thetdotexchange/ContentFragment.java
+++ TheTDotExchange/app/src/main/java/com/example/android/thetdotexchange/ContentFragment.java
@@ -6,18 +6,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Created by Cathy on 2015-09-12.
  */
 public class ContentFragment extends Fragment{
+    public static final int SORT_BY_COURSE_CODE = 0;
+    public static final int SORT_BY_PRICE = 1;
+    public static final int SORT_BY_DISTANCE = 2;
+
     ListView lv;
+    ArrayList<SaleItem> listItems;
+    SearchView searchView;
+    ListViewItemAdapter adapter;
 
     public ContentFragment() {
     }
@@ -27,20 +34,52 @@ public class ContentFragment extends Fragment{
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_content, container, false);
 
-        ArrayList<SaleItem> listRow = new ArrayList<>();
+        listItems = new ArrayList<>();
 
         for(int i = 1; i <= 20; i++) {
-            SaleItem temp = new SaleItem();
-            temp.image = BitmapFactory.decodeResource(getResources(),R.mipmap.cat);
-            temp.textBookTitle = "Test" + i;
-            temp.sellerInfo = "Seller" + i;
-            listRow.add(temp);
+            SaleItem temp = new SaleItem("CourseCode" + i, "Title" + i, Math.random()*100, Math.random()*10);
+            listItems.add(temp);
         }
 
         lv = (ListView)rootView.findViewById(R.id.listView);
-        lv.setAdapter(new ListViewItemAdapter(inflater, listRow));
+        adapter = new ListViewItemAdapter(inflater, listItems);
+        lv.setAdapter(adapter);
+
+        searchView = (SearchView)rootView.findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                performLiveSearch(newText);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (query.length() != 0) {
+                    System.out.println("--->" + query);
+                    // handle search here
+                    performSearch(query);
+                    return true;
+                } else if (query.length() == 0 || query == null){
+                    performSearch("");
+                    return true;
+                }
+                return false;
+            }
+        });
 
         return rootView;
+    }
+
+    public void performSearch(String searchQuery){
+        searchView.clearFocus();
+        adapter.getFilter().filter(searchQuery);
+        return;
+    }
+
+    public void performLiveSearch(String searchQuery) {
+        adapter.getFilter().filter(searchQuery);
+        return;
     }
 
     @Override
@@ -57,5 +96,30 @@ public class ContentFragment extends Fragment{
 
         ListViewItemAdapter adapter = new ListViewItemAdapter(this, lst);
         lv.setAdapter(adapter);*/
+    }
+
+    // 0 == sort by course code
+    // 1 == sort by price
+    // 2 == sort by distance
+    // else sort by course
+    public void sortListBy(final int sortBy){
+        Collections.sort(listItems, new Comparator<SaleItem>(){
+           public int compare(SaleItem item1, SaleItem item2){
+               switch(sortBy) {
+                   case 0:
+                       return item1.courseCode.compareTo(item2.courseCode);
+                   case 1:
+                       if(item1.price < item2.price) return -1;
+                       else if(item1.price == item2.price) return 0;
+                       else return 1;
+                   case 2:
+                       if(item1.distance < item2.distance) return -1;
+                       else if(item1.distance == item2.distance) return 0;
+                       else return 1;
+                   default:
+                       return item1.courseCode.compareTo(item2.courseCode);
+               }
+           }
+        });
     }
 }
--- TheTDotExchange/app/src/main/java/com/example/android/thetdotexchange/ListViewItemAdapter.java
+++ TheTDotExchange/app/src/main/java/com/example/android/thetdotexchange/ListViewItemAdapter.java
@@ -1,29 +1,84 @@
 package com.example.android.thetdotexchange;
 
-import android.content.Context;
-import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.ListAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Bocaj on 12/09/2015.
  */
-public class ListViewItemAdapter extends BaseAdapter {
+public class ListViewItemAdapter extends BaseAdapter implements Filterable {
     private LayoutInflater mInflater;
     private ArrayList<SaleItem> mSaleItems;
+    private ArrayList<SaleItem> storedItems;
+
+    Filter myFilter = new Filter(){
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint){
+            FilterResults results = new FilterResults();
+
+            // perform your search here using the searchConstraint String.
+            constraint = constraint.toString().toLowerCase();
+
+            if (constraint == null || constraint.length() == 0){
+                synchronized (this) {
+                    results.values = storedItems;
+                    results.count = storedItems.size();
+                }
+            } else {
+                ArrayList<SaleItem> filteredArrayNames = new ArrayList<>();
+                ArrayList<SaleItem> unfilteredArrayNames = new ArrayList<>();
+
+                synchronized (this){
+                    unfilteredArrayNames.addAll(storedItems);
+                }
+
+                for (int i = 0; i < unfilteredArrayNames.size(); i++) {
+                    String courseCode = unfilteredArrayNames.get(i).courseCode;
+                    String title = unfilteredArrayNames.get(i).title;
+                    String priceString = unfilteredArrayNames.get(i).priceString;
+                    String distanceString = unfilteredArrayNames.get(i).distanceString;
+
+                    if (courseCode.toLowerCase().indexOf(constraint.toString()) >= 0 ||
+                            title.toLowerCase().indexOf(constraint.toString()) >= 0 ||
+                            priceString.toLowerCase().indexOf(constraint.toString()) >= 0 ||
+                            distanceString.toLowerCase().indexOf(constraint.toString()) >= 0) {
+                        filteredArrayNames.add(unfilteredArrayNames.get(i));
+                    }
+                }
+
+                results.count = filteredArrayNames.size();
+                results.values = filteredArrayNames;
+                Log.e("VALUES", results.values.toString());
+            }
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results){
+            mSaleItems = (ArrayList<SaleItem>) results.values;
+
+            if (results.count > 0){
+                notifyDataSetChanged();
+            } else {
+                notifyDataSetInvalidated();
+            }
+        }
+    };
 
     public ListViewItemAdapter(LayoutInflater contentFragment, ArrayList<SaleItem> listRow) {
         //mInflater = LayoutInflater.from(contentFragment);
         mInflater = contentFragment;
         mSaleItems = listRow;
+        storedItems = new ArrayList<>(listRow);
     }
 
     @Override
@@ -49,9 +104,10 @@ public class ListViewItemAdapter extends BaseAdapter {
         if(convertView == null) {
             view = mInflater.inflate(R.layout.list_item, parent, false);
             holder = new ViewHolder();
-            holder.bookImage = (ImageView)view.findViewById(R.id.imageView1);
-            holder.title = (TextView)view.findViewById(R.id.textView1);
-            holder.sellerInfo = (TextView)view.findViewById(R.id.textView2);
+            holder.courseCode = (TextView)view.findViewById(R.id.courseCode);
+            holder.title = (TextView)view.findViewById(R.id.textbookTitle);
+            holder.price = (TextView)view.findViewById(R.id.price);
+            holder.distance = (TextView)view.findViewById(R.id.distance);
             view.setTag(holder);
         } else {
             view = convertView;
@@ -59,15 +115,19 @@ public class ListViewItemAdapter extends BaseAdapter {
         }
 
         SaleItem item = mSaleItems.get(position);
-        holder.bookImage.setImageBitmap(item.image);
-        holder.title.setText(item.textBookTitle);
-        holder.sellerInfo.setText(item.sellerInfo);
+        holder.courseCode.setText(item.courseCode);
+        holder.title.setText(item.title);
+        holder.price.setText(item.priceString);
+        holder.distance.setText(item.distanceString);
 
         return view;
     }
 
+    public Filter getFilter(){
+        return myFilter;
+    }
+
     private class ViewHolder {
-        public ImageView bookImage;
-        public TextView title, sellerInfo;
+        public TextView courseCode, title, price, distance;
     }
 }
