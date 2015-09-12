86367d715dd7defce6da3178ea1fd4c6181f5965
Cathy Y Fu <cathyyfu@gmail.com>
2015-09-12 02:57:53 -0400

    version 2!

---
+++ TheTDotExchange/app/src/main/java/com/example/android/thetdotexchange/ContentFragment.java
@@ -0,0 +1,23 @@
+package com.example.android.thetdotexchange;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Cathy on 2015-09-12.
+ */
+public class ContentFragment extends Fragment{
+
+        public ContentFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_content, container, false);
+            return rootView;
+        }
+}
