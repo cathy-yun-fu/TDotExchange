d5bc0789c1bc4c87f49231ebb559913fedf31242
BunchaNumbers <jacobhuangyuning@msn.com>
2015-09-13 05:03:15 -0400

    Search and filter works plis although location still needs to be done

--- TheTDotExchange/app/src/main/java/com/example/android/thetdotexchange/ContentFragment.java
+++ TheTDotExchange/app/src/main/java/com/example/android/thetdotexchange/ContentFragment.java
@@ -1,17 +1,23 @@
 package com.example.android.thetdotexchange;
 
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Spinner;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Cathy on 2015-09-12.
@@ -23,8 +29,10 @@ public class ContentFragment extends Fragment{
 
     ListView lv;
     ArrayList<SaleItem> listItems;
+    ArrayList<SaleItem> restoreCopy;
     SearchView searchView;
     ListViewItemAdapter adapter;
+    Spinner spinner;
 
     public ContentFragment() {
     }
@@ -36,20 +44,47 @@ public class ContentFragment extends Fragment{
 
         listItems = new ArrayList<>();
 
-        for(int i = 1; i <= 20; i++) {
-            SaleItem temp = new SaleItem("CourseCode" + i, "Title" + i, Math.random()*100, Math.random()*10);
-            listItems.add(temp);
-        }
-
         lv = (ListView)rootView.findViewById(R.id.listView);
-        adapter = new ListViewItemAdapter(inflater, listItems);
+        adapter = new ListViewItemAdapter(getActivity().getApplicationContext(), listItems);
         lv.setAdapter(adapter);
+
+        sortListBy(SORT_BY_PRICE);
+
+        spinner = (Spinner)rootView.findViewById(R.id.sortSelector);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(l == 0) {
+                    System.out.println("Course Code");
+                    sortListBy(SORT_BY_COURSE_CODE);
+                    adapter.notifyDataSetChanged();
+                }
+                else if(l == 1) {
+                    System.out.println("Price");
+                    sortListBy(SORT_BY_PRICE);
+                    adapter.notifyDataSetChanged();
+                }
+                else if(l == 2) {
+                    System.out.println("Distance");
+                    sortListBy(SORT_BY_DISTANCE);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         searchView = (SearchView)rootView.findViewById(R.id.searchView);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextChange(String newText) {
-                performLiveSearch(newText);
+                if (newText.equals("")) {
+                    performSearch("");
+                    return true;
+                }
                 return false;
             }
 
@@ -57,13 +92,12 @@ public class ContentFragment extends Fragment{
             public boolean onQueryTextSubmit(String query) {
                 if (query.length() != 0) {
                     System.out.println("--->" + query);
-                    // handle search here
                     performSearch(query);
                     return true;
-                } else if (query.length() == 0 || query == null){
+                } /*else if (query.length() == 0) {
                     performSearch("");
                     return true;
-                }
+                }*/
                 return false;
             }
         });
@@ -72,30 +106,36 @@ public class ContentFragment extends Fragment{
     }
 
     public void performSearch(String searchQuery){
-        searchView.clearFocus();
-        adapter.getFilter().filter(searchQuery);
-        return;
-    }
+        //adapter.getFilter().filter(searchQuery);
 
-    public void performLiveSearch(String searchQuery) {
-        adapter.getFilter().filter(searchQuery);
-        return;
-    }
+        listItems.clear();
+        for (SaleItem tempItem : restoreCopy) {
+            listItems.add(new SaleItem(tempItem));
+        }
+        adapter.notifyDataSetChanged();
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        /*ArrayList<SaleItem> lst = new ArrayList<SaleItem>();
-        for(int i = 1; i <= 10; i++) {
-            SaleItem temp = new SaleItem();
-            temp.textBookTitle = "Test" + i;
-            temp.sellerInfo = "Seller" + i;
-            lst.add(temp);
+        if(searchQuery.equals("")){
+            return;
         }
 
-        ListViewItemAdapter adapter = new ListViewItemAdapter(this, lst);
-        lv.setAdapter(adapter);*/
+        searchView.clearFocus();
+        String constraint = searchQuery.toLowerCase();
+
+        for(Iterator<SaleItem> i = listItems.iterator(); i.hasNext();){
+            SaleItem tempItem = i.next();
+            String courseCode = tempItem.courseCode;
+            String title = tempItem.title;
+            String priceString = tempItem.priceString;
+            String distanceString = tempItem.distanceString;
+
+            if (!(courseCode.toLowerCase().contains(constraint) ||
+                    title.toLowerCase().contains(constraint) ||
+                    priceString.toLowerCase().contains(constraint)  ||
+                    distanceString.toLowerCase().contains(constraint))) {
+                i.remove();
+            }
+        }
+        adapter.notifyDataSetChanged();
     }
 
     // 0 == sort by course code
@@ -103,23 +143,63 @@ public class ContentFragment extends Fragment{
     // 2 == sort by distance
     // else sort by course
     public void sortListBy(final int sortBy){
-        Collections.sort(listItems, new Comparator<SaleItem>(){
-           public int compare(SaleItem item1, SaleItem item2){
-               switch(sortBy) {
-                   case 0:
-                       return item1.courseCode.compareTo(item2.courseCode);
-                   case 1:
-                       if(item1.price < item2.price) return -1;
-                       else if(item1.price == item2.price) return 0;
-                       else return 1;
-                   case 2:
-                       if(item1.distance < item2.distance) return -1;
-                       else if(item1.distance == item2.distance) return 0;
-                       else return 1;
-                   default:
-                       return item1.courseCode.compareTo(item2.courseCode);
-               }
-           }
-        });
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("SaleItem");
+        switch(sortBy){
+            case SORT_BY_COURSE_CODE:
+                query.orderByAscending("courseCode");
+                query.setLimit(20);
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    public void done(List<ParseObject> itemList, ParseException e) {
+                        if (e == null) {
+                            listItems.clear();
+                            for (int i = 0; i < itemList.size(); i++) {
+                                ParseObject temp = itemList.get(i);
+                                String cCode = temp.get("courseCode").toString();
+                                String title = temp.get("title").toString();
+                                double price = Double.parseDouble(temp.get("price").toString());
+                                double lat = Double.parseDouble(temp.get("Lat").toString());
+                                double lon = Double.parseDouble(temp.get("Lon").toString());
+                                SaleItem toAdd = new SaleItem(cCode, title, price, lat, lon);
+                                listItems.add(toAdd);
+                            }
+                            restoreCopy = new ArrayList<>();
+                            restoreCopy.addAll(listItems);
+                            adapter.notifyDataSetChanged();
+                        } else {
+                            System.out.println("Error: " + e.getMessage());
+                        }
+                    }
+                });
+                break;
+            case SORT_BY_PRICE:
+                query.orderByAscending("price");
+                query.setLimit(20);
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    public void done(List<ParseObject> itemList, ParseException e) {
+                        if (e == null) {
+                            listItems.clear();
+                            for (int i = 0; i < itemList.size(); i++) {
+                                ParseObject temp = itemList.get(i);
+                                String cCode = temp.get("courseCode").toString();
+                                String title = temp.get("title").toString();
+                                double price = Double.parseDouble(temp.get("price").toString());
+                                double lat = Double.parseDouble(temp.get("Lat").toString());
+                                double lon = Double.parseDouble(temp.get("Lon").toString());
+                                SaleItem toAdd = new SaleItem(cCode, title, price, lat, lon);
+                                listItems.add(toAdd);
+                            }
+                            restoreCopy = new ArrayList<>();
+                            restoreCopy.addAll(listItems);
+                            adapter.notifyDataSetChanged();
+                        } else {
+                            System.out.println("Error: " + e.getMessage());
+                        }
+                    }
+                });
+                break;
+            case SORT_BY_DISTANCE:
+                break;
+            default:
+        }
     }
 }
--- TheTDotExchange/app/src/main/java/com/example/android/thetdotexchange/SaleItem.java
+++ TheTDotExchange/app/src/main/java/com/example/android/thetdotexchange/SaleItem.java
@@ -14,8 +14,13 @@ public class SaleItem {
     String distanceString;
     double price;
     double distance;
+    double latitude;
+    double longitude;
 
-    SaleItem(String courseCode, String title, double price, double distance){
+    static double myLat = 0;
+    static double myLon = 0;
+
+    SaleItem(String courseCode, String title, double price, double lat, double lon){
         DecimalFormat df = new DecimalFormat("#.##");
         this.courseCode = courseCode;
         this.title = title;
@@ -23,9 +28,55 @@ public class SaleItem {
         if(price < 0) price = 0;
 
         this.price = price;
-        this.distance = distance;
 
         priceString = "$" + df.format(price);
+
+        latitude = lat;
+        longitude = lon;
+
+        distance = distance(myLat, myLon, lat, lon, 'K');
         distanceString = df.format(distance) + " km";
+    }
+
+    public SaleItem(SaleItem another) {
+        this.courseCode = another.courseCode;
+        this.title = another.title;
+        this.priceString = another.priceString;
+        this.distanceString = another.distanceString;
+        this.price = another.price;
+        this.distance = another.distance;
+        this.latitude = another.latitude;
+        this.longitude = another.longitude;
+    }
+
+    private double distance(double lat1, double lon1, double lat2, double lon2, char unit) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        // Kilometers
+        if (unit == 'K') {
+            dist = dist * 1.609344;
+        }
+        // Nautical Mile
+        else if (unit == 'N') {
+            dist = dist * 0.8684;
+        }
+        return (dist);
+    }
+
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts decimal degrees to radians             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts radians to decimal degrees             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
     }
 }
